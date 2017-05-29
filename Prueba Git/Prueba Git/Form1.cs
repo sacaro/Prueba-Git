e03335b51fa2c40472ee0ab5ef1cd365e855861b
sacaro <sacr18@hotmail.com>
2017-05-29 11:55:34 -0600

    solucion completa

--- Prueba Git/Prueba Git/Form1.cs
+++ Prueba Git/Prueba Git/Form1.cs
@@ -16,5 +16,10 @@ namespace Prueba_Git
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //
+        }
     }
 }
